c1ec62d7802d45f3755a054222a2489c16b3908a
parashuram-patil <parasharam_patil@ymail.com>
2020-04-10 18:20:30 +0530

    Componet test cases > npm test

--- tests/integration/components/jumbo-test.js
+++ tests/integration/components/jumbo-test.js
@@ -3,24 +3,14 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | jumbo', function(hooks) {
+module('Integration | Component | jumbo', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it renders the content inside a jumbo header with a tomster', async function (assert) {
 
-    await render(hbs`<Jumbo />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <Jumbo>
-        template block text
-      </Jumbo>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    await render(hbs`<Jumbo>Hello World</Jumbo>`);
+    assert.dom('.jumbo').exists();
+    assert.dom('.jumbo').hasText('Hello World');
+    assert.dom('.jumbo .tomster').exists();
   });
 });
