c1500152e748179b25d97070690e933843f01715
parashuram-patil <parasharam_patil@ymail.com>
2020-04-10 18:43:18 +0530

    Test Cases for Nav Bar

---
+++ tests/integration/components/nav-bar-test.js
@@ -0,0 +1,20 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | nav-bar', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<NavBar />`);
+
+    assert.dom('.menu ').hasText('SuperRentals About Contact');
+    assert.dom('.menu .menu-index').hasText('SuperRentals');
+    assert.dom('.menu .menu-about').hasText('About');
+    assert.dom('.menu .menu-contact').hasText('Contact');
+  });
+});
